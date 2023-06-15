1ca5cb036ef5dc379dd3761fd75cc494e1e038a5
Fillah Alamsyah <87614211+FillahAlamsyah@users.noreply.github.com>
2023-06-15 19:51:15 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,64 @@
-from mtcnn.mtcnn import MTCNN
+import cv2
+import streamlit as st
+import numpy as np
+
+
+def brighten_image(image, amount):
+    img_bright = cv2.convertScaleAbs(image, beta=amount)
+    return img_bright
+
+
+def enhance_details(img):
+    hdr = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
+    return hdr
+
+
+def detect_faces(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
+
+
+def draw_face_boxes(image, faces):
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return image
+
+
+def main_loop():
+    st.title("OpenCV Demo App")
+    st.subheader("Aplikasi ini memungkinkan Anda untuk memanipulasi filter gambar dan melakukan face recognition menggunakan OpenCV dan Streamlit")
+
+    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
+    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
+    apply_face_recognition = st.sidebar.checkbox('Face Recognition')
+
+    video_capture = cv2.VideoCapture(0)
+    frame_placeholder = st.empty()
+
+    while True:
+        _, frame = video_capture.read()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        processed_image = brighten_image(frame_rgb, brightness_amount)
+
+        if apply_enhancement_filter:
+            processed_image = enhance_details(processed_image)
+
+        if apply_face_recognition:
+            faces = detect_faces(frame)
+            processed_image = draw_face_boxes(processed_image, faces)
+
+        frame_placeholder.image(processed_image)
+
+    video_capture.release()
+
+
+if __name__ == '__main__':
+    main_loop()
+
+'''from mtcnn.mtcnn import MTCNN
 import streamlit as st
 import matplotlib.pyplot as plt 
 from matplotlib.patches import Rectangle
@@ -82,3 +142,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
