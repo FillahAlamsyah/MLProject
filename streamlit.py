3d2058ff185b30009bc42d666aef5ac8fb8db1db
Fillah Alamsyah <87614211+FillahAlamsyah@users.noreply.github.com>
2023-06-15 18:50:14 +0700

    Update streamlit.py
    Fix Indentation Error

--- streamlit.py
+++ streamlit.py
@@ -34,12 +34,12 @@ def main():
                 for _, value in face['keypoints'].items():
                     dot = Circle(value, radius=2, color='maroon')
                     ax.add_patch(dot)
-             st.pyplot(fig)
+            st.pyplot(fig)
             
-             results = detector.detect_faces(pixels)
-             x1, y1, width, height = results[0]["box"]
-             x2, y2 = x1 + width, y1 + height
-             face = pixels[y1:y2, x1:x2]
+            results = detector.detect_faces(pixels)
+            x1, y1, width, height = results[0]["box"]
+            x2, y2 = x1 + width, y1 + height
+            face = pixels[y1:y2, x1:x2]
     
     elif choice == "Face Detection 2":
             uploaded_file = st.file_uploader("Choose File", type=["jpg","png"])
